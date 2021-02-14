5706db6239011a4905bc90409b4702cf7f2f485a
pi@rpi-gdoor <pi@rpi-gdoor>
2021-02-13 17:55:41 -0800

    don't set a bitrate

--- client/client.sh
+++ client/client.sh
@@ -3,4 +3,4 @@ source $(dirname $0)/../common.sh
 
 log "starting"
 
-loop_command iperf3 -B $CLIENT_ADDR -c $SERVER_ADDR -u -t $CLIENT_RUN_DURATION_SECS -b $BITRATE
+loop_command iperf3 -B $CLIENT_ADDR -c $SERVER_ADDR -u -t $CLIENT_RUN_DURATION_SECS #-b $BITRATE
